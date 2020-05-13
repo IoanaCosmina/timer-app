93907333ccf95c3b6672ceb0ca65568f39815f57
unknown <andriescuioana@gmail.com>
2020-05-13 15:05:36 +0200

    base structure and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function padTime(time) {
+  return time.toString().padStart(2, '0');
+}
+
 function App() {
+  const [title, setTitle] = useState('Ready, set, go!');
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>{title}</h2>
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div className="buttons">
+        <button>
+          <span>Start</span>
+          <i className="fa fa-play" aria-hidden="true"></i>
+        </button>
+        <button>
+          <span>Stop</span>
+          <i className="fa fa-stop" aria-hidden="true"></i>
+        </button>
+        <button>
+          <span>Reset</span>
+          <i className="fa fa-undo" aria-hidden="true"></i>
+        </button>
+      </div>
     </div>
   );
 }
