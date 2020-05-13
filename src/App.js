f7164c14147afba13a5d6c89f930794a198d7242
unknown <andriescuioana@gmail.com>
2020-05-13 20:51:08 +0200

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,93 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import './App.css';
-import soundfile from './assets/gongSound.mp3';
+import Timer from './Timer';
 
-function padTime(time) {
-  return time.toString().padStart(2, '0');
-}
-
-function App() {
-  const duration = 25;
-  const [title, setTitle] = useState('Ready, set, go!');
-  const [timeLeft, setTimeLeft] = useState(duration * 60);
-  const [isRunning, setIsRunning] = useState(false);
-  const [isComplete, setIsComplete] = useState(false);
-  const intervalRef = useRef(null);
-
-  const minutes = padTime(Math.floor(timeLeft / 60));
-  const seconds = padTime(timeLeft - minutes * 60);
-
-  function startTimer() {
-    // it means we already have a timer started
-    if (intervalRef.current !== null) return;
-
-    setTitle('You can do it!');
-    setIsRunning(true);
-    setIsComplete(false);
-    intervalRef.current = setInterval(() => {
-      setTimeLeft(timeLeft => {
-        // stop the counter from getting below 0
-        if (timeLeft >= 1) return timeLeft - 1;
-
-        setIsComplete(true);
-        // reset the timer when the countdown is finished
-        resetTimer();
-        return 0;
-      });
-    }, 1000);
-  };
-
-  function stopTimer() {
-    // there is no timer started
-    if (intervalRef.current === null) return;
-
-    setTitle('Keep it up!');
-    clearInterval(intervalRef.current);
-    // without null, the startTimer won't start again
-    intervalRef.current = null;
-    setIsRunning(false);
-  };
-
-  function resetTimer() {
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
-    setTitle('Ready to go another round?');
-    setTimeLeft(duration * 60);
-    setIsRunning(false);
-  };
-
+export default function App() {
   return (
     <div className="app">
-      <h2>{title}</h2>
-      <div className="timer">
-        <span>{minutes}</span>
-        <span>:</span>
-        <span>{seconds}</span>
-      </div>
-      {
-        isComplete && <audio src={soundfile} autoPlay />
-      }
-      <div className="buttons">
-        {!isRunning &&
-          <button onClick={startTimer}>
-            <span>Start</span>
-            <i className="fa fa-play" aria-hidden="true"></i>
-          </button>
-        }
-        {
-          isRunning &&
-          <button onClick={stopTimer}>
-            <span>Stop</span>
-            <i className="fa fa-stop" aria-hidden="true"></i>
-          </button>
-        }
-        <button onClick={resetTimer}>
-          <span>Reset</span>
-          <i className="fa fa-undo" aria-hidden="true"></i>
-        </button>
-      </div>
+      <Timer />
     </div>
   );
 };
-
-export default App;
