34a6c09e83c37037c052c1d8c684d2681d07e353
unknown <andriescuioana@gmail.com>
2020-05-13 20:54:13 +0200

    Updates

---
+++ precache-manifest.3640adbfa207f27cae971b58ea894de1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a32a249cb0b3eaea3011bf55bc9268a",
+    "url": "/timer-app/index.html"
+  },
+  {
+    "revision": "83af2b2a22e67ceb0220",
+    "url": "/timer-app/static/css/main.6508734f.chunk.css"
+  },
+  {
+    "revision": "b40bb487b4e7afb2b512",
+    "url": "/timer-app/static/js/2.3fbfd20d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/timer-app/static/js/2.3fbfd20d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83af2b2a22e67ceb0220",
+    "url": "/timer-app/static/js/main.bb372b34.chunk.js"
+  },
+  {
+    "revision": "b5ab0fd9a04e9212d366",
+    "url": "/timer-app/static/js/runtime-main.78870df5.js"
+  },
+  {
+    "revision": "16e6a7d8abfdeb19d7e5350f5bed3935",
+    "url": "/timer-app/static/media/gongSound.16e6a7d8.mp3"
+  }
+]);
